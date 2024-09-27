a71e2c14b36a2b108fb934ec5f9eda6749d7225c
surfiniaburger <jdmasciano2@gmail.com>
2024-09-27 05:03:36 +0100

    color codes

--- src/components/health-record.tsx
+++ src/components/health-record.tsx
@@ -41,7 +41,7 @@ const HealthRecordVerification = () => {
   };
 
   return (
-    <div className="h-[50rem] w-full dark:bg-black bg-gray-900 dark:bg-dot-white/[0.1] bg-dot-white/[0.1] relative flex items-center justify-center">
+    <div className="h-[50rem] w-full dark:bg-black bg-gray-900 dark:bg-dot-white/[0.3] bg-dot-white/[0.1] relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-gray-800 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="glass-card relative z-20">
